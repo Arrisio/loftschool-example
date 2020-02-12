8880c98ee68beb0f0948e30cd9d6e2b692b57f08
dsuchkov <dsuchkov@jet.su>
2020-02-12 18:33:11 +0300

    tmp commit

--- src/dnd.js
+++ src/dnd.js
@@ -27,6 +27,23 @@ const homeworkContainer = document.querySelector('#homework-container');
    homeworkContainer.appendChild(newDiv);
  */
 function createDiv() {
+    const div = document.createElement('div');
+
+    div.classList.add('draggable-div');
+    div.setAttribute('draggable', 'true');
+    let divId = 'div' + Math.floor(Math.random() * 100);
+
+    div.setAttribute('id', divId);
+    div.style.border = '2px solid black';
+    div.style.backgroundColor =
+        '#' + Math.floor(Math.random() * 16777215).toString(16);
+    div.style.height = Math.floor(Math.random() * 300) + 'px';
+    div.style.width = Math.floor(Math.random() * 300) + 'px';
+    div.style.top = Math.floor(Math.random() * 100) + 'px';
+    div.style.left = Math.floor(Math.random() * 500) + 'px';
+    div.style.position = 'relative';
+
+    return div;
 }
 
 /*
@@ -37,7 +54,51 @@ function createDiv() {
    homeworkContainer.appendChild(newDiv);
    addListeners(newDiv);
  */
+
+let dragSrcEl = null;
+let dragSrcElStyle = {};
+
 function addListeners(target) {
+    let handlerDragStart = e => {
+        e.target.style.opacity = '0.4';
+        dragSrcEl = e.target;
+        dragSrcElStyle = { ...dragSrcEl.style };
+        e.dataTransfer.effectAllowed = 'move';
+        // e.dataTransfer.setData('text', e.target.innerHTML);
+    };
+    let handlerDragOver = e => {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = 'move';
+    };
+    let handlerDragEnter = e => {
+        e.target.style.border = '2px dashed black';
+    };
+    let handlerDragLeave = e => {
+        e.target.style.border = '2px solid black';
+    };
+    let handlerDragEnd = e => {
+        e.target.style.opacity = 1;
+    };
+    let handlerDrop = e => {
+        e.stopPropagation();
+        e.target.style.border = '2px solid black';
+        if (dragSrcEl != e.target) {
+            dragSrcEl.style.width = e.target.style.width;
+            dragSrcEl.style.height = e.target.style.height;
+            dragSrcEl.style.backgroundColor = e.target.style.backgroundColor;
+            e.target.style.width = dragSrcElStyle.width;
+            e.target.style.height = dragSrcElStyle.height;
+            e.target.style.backgroundColor = dragSrcElStyle.backgroundColor;
+            // e.target.innerHTML = e.dataTransfer.getData('text/html');
+        }
+    };
+
+    target.addEventListener('dragstart', handlerDragStart, false);
+    target.addEventListener('dragenter', handlerDragEnter, false);
+    target.addEventListener('dragover', handlerDragOver, false);
+    target.addEventListener('dragleave', handlerDragLeave, false);
+    target.addEventListener('dragend', handlerDragEnd, false);
+    target.addEventListener('drop', handlerDrop, false);
 }
 
 let addDivButton = homeworkContainer.querySelector('#addDiv');
