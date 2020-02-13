e953794f8551bfa11d0f7a3a2df379f0bb2993bd
Arrisio <zemlak@list.ru>
2020-02-13 23:03:45 +0300

    tmp commit

--- src/dnd.js
+++ src/dnd.js
@@ -26,37 +26,43 @@ const homeworkContainer = document.querySelector('#homework-container');
    const newDiv = createDiv();
    homeworkContainer.appendChild(newDiv);
  */
+
+const uuidv1 = require('uuid/v1');
+
 function createDiv() {
 
-    function getRandomColor() {
-        let letters = '0123456789ABCDEF';
-        let color = '#';
-        for (var i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
+  function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
     }
+    return color;
+  }
 
-    function setRandomSize(element, maxHeight = 240, maxWidth = 240,) {
-        element.style.height = Math.floor(Math.random() * maxHeight) + 'px';
-        element.style.width = Math.floor(Math.random() * maxWidth) + 'px';
-    }
+  function setRandomSize(element, maxHeight = 240, maxWidth = 240,) {
+    element.style.height = Math.floor(Math.random() * maxHeight) + 'px';
+    element.style.width = Math.floor(Math.random() * maxWidth) + 'px';
+  }
 
-    function setRandomPosition(element) {
-        element.style.left = Math.floor(Math.random() * (homeworkContainer.clientWidth - element.clientWidth)) + 'px';
-        element.style.top = Math.floor(Math.random() * (homeworkContainer.clientHeight - element.clientHeight)) + 'px';
-        element.style.position = 'absolute';
-    }
+  function setRandomPosition(element) {
+    element.style.left = Math.floor(Math.random() * (homeworkContainer.clientWidth - element.clientWidth)) + 'px';
+    element.style.top = Math.floor(Math.random() * (homeworkContainer.clientHeight - element.clientHeight)) + 'px';
+    element.style.position = 'absolute';
+  }
 
-    const div = document.createElement('div');
+  const div = document.createElement('div');
 
-    div.classList.add('draggable-div');
-    div.setAttribute('draggable', 'true');
-    div.style.backgroundColor = getRandomColor();
-    setRandomSize(div)
-    setRandomPosition(div);
+  div.classList.add('draggable-div');
+  div.setAttribute('draggable', 'true');
+  div.style.backgroundColor = getRandomColor();
+  setRandomSize(div)
+  setRandomPosition(div);
 
-    return div;
+  div.setAttribute('id', uuidv1());
+  div.setAttribute('z-index', 0);
+
+  return div;
 }
 
 /*
@@ -69,35 +75,68 @@ function createDiv() {
  */
 
 function addListeners(target) {
-    let handlerDragStart = e => {
-        e.target.style.opacity = '0.4';
-        console.log('dragstart Реализовано согласно ТЗ ');
-    };
-    let handlerDragEnd = e => {
-        // e.preventDefault();
-        e.target.style.opacity = 1;
-        console.log('dragend Реализовано согласно ТЗ ');
-    };
+  let handlerDragStart = e => {
+    e.target.style.opacity = '0.4';
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('divId', e.target.id);
+    e.dataTransfer.setData('clientX', e.clientX);
+    e.dataTransfer.setData('clientY', e.clientY);
+  };
+  let handlerDragOver = e => {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
+  };
+  let handlerDragEnter = e => {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
+  };
+  let handlerDragEnd = e => {
+    e.preventDefault();
+    e.target.style.opacity = 1;
+    e.target.style.zIndex = getTopZIndex();
+  };
+  let handlerDrop = e => {
+    e.preventDefault();
+    let srcElem = document.getElementById(e.dataTransfer.getData('divId'));
+    srcElem.style.left = e.clientX - e.dataTransfer.getData('clientX') + srcElem.getBoundingClientRect().x + 'px';
+    srcElem.style.top = e.clientY - e.dataTransfer.getData('clientY') + srcElem.getBoundingClientRect().y + 'px';
+    e.dataTransfer.clearData();
+  };
 
-    target.addEventListener('dragstart', handlerDragStart, false);
-    target.addEventListener('dragend', handlerDragEnd, false);
-    // target.addEventListener('drop', handlerDrop, false);
+  target.addEventListener('dragstart', handlerDragStart, false);
+  target.addEventListener('dragend', handlerDragEnd, false);
+
+  target.parentNode.addEventListener('dragenter', handlerDragEnter, false);
+  target.parentNode.addEventListener('dragover', handlerDragOver, false);
+  target.parentNode.addEventListener('drop', handlerDrop, false);
+  target.parentNode.style.height = '100vh';
 }
 
 let addDivButton = homeworkContainer.querySelector('#addDiv');
 
-addDivButton.addEventListener('click', function() {
-    // создать новый div
-    const div = createDiv();
+addDivButton.addEventListener('click', function () {
+  // создать новый div
+  const div = createDiv();
 
-    // добавить на страницу
-    homeworkContainer.appendChild(div);
-    // назначить обработчики событий мыши для реализации D&D
-    addListeners(div);
-    // можно не назначать обработчики событий каждому div в отдельности, а использовать делегирование
-    // или использовать HTML5 D&D - https://www.html5rocks.com/ru/tutorials/dnd/basics/
+  // добавить на страницу
+  homeworkContainer.appendChild(div);
+  // назначить обработчики событий мыши для реализации D&D
+  addListeners(div);
+  // можно не назначать обработчики событий каждому div в отдельности, а использовать делегирование
+  // или использовать HTML5 D&D - https://www.html5rocks.com/ru/tutorials/dnd/basics/
 });
 
+function getTopZIndex() {
+  let highestZIdx = -99999;
+  for (let div of homeworkContainer.children) {
+    if (div.style.zIndex > highestZIdx) {
+      highestZIdx = div.style.zIndex;
+    }
+  }
+
+  return ++highestZIdx
+}
+
 export {
-    createDiv
+  createDiv
 };
