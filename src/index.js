94518ef5d7c0f1d7865eef76e5fbcc6aa628ec95
dsuchkov <dsuchkov@jet.su>
2020-02-17 11:23:50 +0300

    fixed claims

--- src/index.js
+++ src/index.js
@@ -77,7 +77,12 @@ function delegate(target, fn) {
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
-    target.addEventListener('click', fn, { once: true });
+    let handler = () => {
+        fn();
+        target.removeEventListener('click', handler);
+    }
+
+    target.addEventListener('click', handler);
 }
 
 export {
